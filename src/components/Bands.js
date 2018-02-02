b818f98453e147b30f35d18a4a33d8a9b338c764
Monika Williams <monika.m.williams@gmail.com>
2018-02-02 12:45:14 +0000

    automatically backed up by learn

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 
 class Bands extends Component {
   render() {
-    const todos = this.props.store.getState().todos.map((todo, index) => {
-      return <Todo text={todo.text} key={index} id={todo.id} /* <- code change */ store={this.props.store} /> 
+    const bands = this.props.store.getState().bands.map((band, index) => {
+      return <Band text={band.text} key={index} id={band.id} store={this.props.store} />
     });
- 
+
     return(
       <ul>
         band={band}
