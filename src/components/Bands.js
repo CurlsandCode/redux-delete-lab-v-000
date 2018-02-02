612c5d61eb37cd845362af7f153137a43dcf9a5e
Monika Williams <monika.m.williams@gmail.com>
2018-02-02 12:44:03 +0000

    automatically backed up by learn

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 
 class Bands extends Component {
   render() {
+    const todos = this.props.store.getState().todos.map((todo, index) => {
+      return <Todo text={todo.text} key={index} id={todo.id} /* <- code change */ store={this.props.store} /> 
+    });
+ 
     return(
       <ul>
-        Bands Component
+        band={band}
       </ul>
     );
   }
